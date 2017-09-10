b77dbf74262ad1c9d95c72570dc0c3e5cd6862a1
jeimison3 <jeimison3@gmail.com>
2017-09-10 00:34:35 -0300

    Mudança no error404

--- err404.php
+++ err404.php
@@ -3,6 +3,7 @@ class WebView{
   public static function html(){
     CAPISPHP_Structure::setTitl("Erro 404");
     return "<center><h2>Ops... A página que tentou acessar não existe :/</h2></center>";
+
   }
 }
 ?>

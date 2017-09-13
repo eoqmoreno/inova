f6f61211d219433fdfc1f1f794e3f0a90ff8380f
jeimison3 <jeimison3@gmail.com>
2017-09-13 20:15:47 -0300

    Mapa aproximado 2x e satélite ativo

--- js/main.js
+++ js/main.js
@@ -157,11 +157,13 @@ jQuery(function($) {
 	function initialize_map() {
 		var myLatlng = new google.maps.LatLng(latitude,longitude);
 		var mapOptions = {
-			zoom: 14,
+			zoom: 17,
 			scrollwheel: false,
+			mapTypeId: 'satellite',
 			center: myLatlng
 		};
 		var map = new google.maps.Map(document.getElementById('google-map'), mapOptions);
+
 		var contentString = '';
 		var infowindow = new google.maps.InfoWindow({
 			content: '<div class="map-content"><ul class="address">' + $('.address').html() + '</ul></div>'
@@ -173,6 +175,7 @@ jQuery(function($) {
 		google.maps.event.addListener(marker, 'click', function() {
 			infowindow.open(map,marker);
 		});
+
 	}
 	google.maps.event.addDomListener(window, 'load', initialize_map);
 

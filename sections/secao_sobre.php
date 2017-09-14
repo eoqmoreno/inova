691ee7ddcdac1cf7e4d73d850d24f863f736ca75
jeimison3 <jeimison3@gmail.com>
2017-09-14 13:29:47 -0300

    Cores de 'sobre' corrigidas, itens organizados.

--- sections/secao_sobre.php
+++ sections/secao_sobre.php
@@ -2,35 +2,28 @@
 
 ?>
   <section id="sobre-nos" class="parallax">
-    <div class="container" style="background: rgba(2, 143, 204, 0.67);box-shadow: 0px 0px 0px 5px rgba(255, 141, 6, 0.4);border-radius: 25px;padding: 10px;">
+    <div class="container" style="background: rgba(2, 143, 204, 0.9);box-shadow: 0px 0px 0px 5px rgba(255, 141, 6, 0.2);border-radius: 25px;padding: 10px;">
       <div style="width:100%;">
         <div class="col-sm-12 col-xs-12">
           <div class="about-info wow fadeInUp" data-wow-duration="1000ms" data-wow-delay="300ms">
-            <div class="col-sm-6 col-xs-12">
-            <h2>Sobre nós</h2>
-            <p>A INOVA – Indústria e Comércio de Utilidades para o lar, foi fundada em 31 de outubro de 2015. O corpo técnico é composto por profissionais das áreas de Engenharia, Administração, Recursos Humanos, Ciências Contábeis, e demais com formação técnica nas áreas de Eletrotécnica e Mecânica Industrial.</p>
-            <p>A empresa tem se baseado na busca constante pela qualidade, na viabilização de uma gestão dinâmica e eficiente e, sobretudo, no compromisso em desenvolver seus profissionais.</p>
-<!--<ul class="nostyle">
-  <li><h2><i class="glyphicon glyphicon-chevron-right"></i> MISSÃO</h2></li>
-</ul>-->
-<br/>
-<h2>Missão</h2>
-<p>Ofertar ao mercado de utilidades plásticas, produtos acessíveis com garantia de qualidade, com foco em gestão de resultados e desenvolvimento das pessoas, gerando valor ao negócio.</p>
 
-<!--<ul class="nostyle">
-    <li><h2><i class="glyphicon glyphicon-chevron-right"></i> VISÃO</h2></li>
-</ul>-->
+<div class="col-xs-12">
+<h2>Sobre nós</h2>
+<p>A INOVA – Indústria e Comércio de Utilidades para o lar, foi fundada em 31 de outubro de 2015. O corpo técnico é composto por profissionais das áreas de Engenharia, Administração, Recursos Humanos, Ciências Contábeis, e demais com formação técnica nas áreas de Eletrotécnica e Mecânica Industrial.</p>
+<p>A empresa tem se baseado na busca constante pela qualidade, na viabilização de uma gestão dinâmica e eficiente e, sobretudo, no compromisso em desenvolver seus profissionais.</p>
 </div>
-<div class="col-sm-6 col-xs-12">
-<br/>
+
+<div class="col-xs-12 col-sm-4">
 <h2>Visão</h2>
 <p>Ser referência no mercado consumidor Norte-Nordeste na comercialização de produtos em utilidades para o lar.</p>
+</div>
 
-<!--<ul class="nostyle">
-  <li><h2><i class="glyphicon glyphicon-chevron-right"></i> VALORES</h2></li>
-</ul>-->
+<div class="col-xs-6 col-sm-4">
+<h2>Missão</h2>
+<p>Ofertar ao mercado de utilidades plásticas, produtos acessíveis com garantia de qualidade, com foco em gestão de resultados e desenvolvimento das pessoas, gerando valor ao negócio.</p>
+</div>
 
-<br/>
+<div class="col-xs-6 col-sm-4">
 <h2>Valores</h2>
 <p><ul class="fa-ul">
   <li><i class="fa-li fa fa-long-arrow-right"></i>Clientes satisfeitos</li>
